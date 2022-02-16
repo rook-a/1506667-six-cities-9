d9e1f5c95d8f380c7118bf964e7ae5ebdb0be02e
rook-a <zhernovkov_konst@mail.ru>
2022-02-16 15:22:32 +0300

    refactor: сделает из стилей модуль

--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import './not-found.css';
+import styles from './not-found.module.css';
 import Header from '../../components/header/header';
 
 function NotFound(): JSX.Element {
@@ -7,15 +7,15 @@ function NotFound(): JSX.Element {
 
   return (
     <div className="page page--gray">
-      <Header isLogged />
+      <Header isLogged={false} />
 
-      <main className="page__error error">
+      <main className={`${styles.page__error} error`}>
         <section>
-          <img className="error__img" src="img/oops.svg" alt="Sad emoji" width={200} height={200} />
-          <h1 className="error__title">Oops...</h1>
-          <p className="error__text">That page can&#39;t be found</p>
+          <img className={`${styles.error__img}`} src="img/oops.svg" alt="Sad emoji" width={200} height={200} />
+          <h1 className={`${styles.error__title}`}>Oops...</h1>
+          <p className={`${styles.error__text}`}>That page can&#39;t be found</p>
 
-          <div className="error__buttons">
+          <div className={`${styles.error__buttons}`}>
             <button
               className="locations__item-link tabs__item--active button"
               onClick={() => navigate('/', { replace: true })}>
