5acf45cc9c407ce614bdcd81b04baebc335ed36d
rook-a <zhernovkov_konst@mail.ru>
2022-04-05 22:50:43 +0300

    refactor: вынесет логирование ошибок из switch. удалит лишнее

--- project/src/services/handle-error.ts
+++ project/src/services/handle-error.ts
@@ -9,23 +9,20 @@ export const handleError = (error: AxiosError): void => {
   }
 
   const { response } = error;
+  rollbar.error(error);
 
   if (response) {
     switch (response.status) {
       case HttpCode.BadRequest:
-        rollbar.error(error);
         toast.info(response.data.error);
         break;
       case HttpCode.Unauthorized:
-        rollbar.error(error);
         toast.info('You are not logged in. Not all features of the app are available');
         break;
       case HttpCode.NotFound:
-        rollbar.error(error);
         toast.error(`This page not found. Try again`);
         break;
       default:
-        rollbar.error(error);
         toast.error('Sorry. Server error or unknown error. Try again later');
     }
   }
